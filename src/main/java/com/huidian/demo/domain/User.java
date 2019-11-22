5475b1867bff3f58cb98450a185767ef9cd060df
sunhaidong <1759013031@qq.com>
2019-11-22 16:51:59 +0800

    第一次上传

---
+++ src/main/java/com/huidian/demo/domain/User.java
@@ -0,0 +1,49 @@
+package com.huidian.demo.domain;
+
+public class User{
+    private Integer uid;
+    private  String uusername;
+    private String upassword;
+
+    public User(Integer uid, String uusername, String upassword) {
+        this.uid = uid;
+        this.uusername = uusername;
+        this.upassword = upassword;
+    }
+
+    public User() {
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getUusername() {
+        return uusername;
+    }
+
+    public void setUusername(String uusername) {
+        this.uusername = uusername;
+    }
+
+    public String getUpassword() {
+        return upassword;
+    }
+
+    public void setUpassword(String upassword) {
+        this.upassword = upassword;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "uid=" + uid +
+                ", uusername='" + uusername + '\'' +
+                ", upassword='" + upassword + '\'' +
+                '}';
+    }
+}
